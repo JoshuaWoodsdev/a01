49e6a0417f5c183c929fcc8bacc9432859a55ffd
Joshua Woods <nordev@protonmail.com>
2022-10-01 15:09:34 -0500

    setup of playground

---
+++ components/assets/cursor-listener.js
@@ -0,0 +1,10 @@
+AFRAME.registerComponent('cursor-listener', {
+    init: function () {
+        var el = this.el;
+        el.addEventListener('click', function (evt) {
+            el.parentNode.removeChild(el);
+        });
+    }
+});
+
+//This connects the controller to item
---
+++ components/assets/raycaster-autorefresh.js
@@ -0,0 +1,11 @@
+AFRAME.registerComponent('raycaster-autorefresh', {
+    init: function () {
+        var el = this.el;
+        this.el.addEventListener('model-loaded', function () {
+            var cursorEl = el.querySelector('[raycaster]');
+            cursorEl.components.raycaster.refreshObjects();
+        });
+    }
+});
+
+//Custom components have to be registered by calling AFRAME.registerComponent() 
